56f4da15a5d247d68a3b91fbfec3e7aef99808fb
davidung221 <davidung221@outlook.com>
2026-09-01 12:57:01 +1000

    Update top_10_per_year.sql

--- sql_queries/top_10_per_year.sql
+++ sql_queries/top_10_per_year.sql
@@ -1,3 +1,17 @@
+/* 
+==============================================================================================
+This query shows the top 10 principal network names by number of incidents per year.
+==============================================================================================
+1. First CTE gets the number of incidents for each principal network name per year.
+2. Second CTE 'ranked' uses dense_rank() to find the top 10.
+3. Final SQL query returns the top 10 for each year.
+
+E.g.,
+YEAR | PRINCIPAL_NETWORK_NAME | NUMBER_OF_INCIDENTS | RNK
+2019 | GLENFIELD | 94 | 1
+==============================================================================================
+*/
+
 with cte as (select
     EXTRACT(year from date_opened) as year,
     principal_network_name,
